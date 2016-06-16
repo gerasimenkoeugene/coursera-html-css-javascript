d89e234726ea60e2066f157108f0b0bb221c2f30
Ievgenii Gerasimenko <iege0316@netcracker.com>
2016-06-16 16:47:47 +0300

    reslove task for module 4

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -9,13 +9,13 @@
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 (function (window) { 
-	var speakWord = "Hello";
+	var speakWord = "Hello ";
 	var helloSpeaker = {};
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-helloSpeaker.speak = function () {
+helloSpeaker.speak = function (name) {
     console.log(speakWord  + name);
   }
 
